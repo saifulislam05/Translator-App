e482f9d90de7782c92774b8251d96a097679904e
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-07 22:20:42 +0530

    init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div className="bg-primary">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Header;
